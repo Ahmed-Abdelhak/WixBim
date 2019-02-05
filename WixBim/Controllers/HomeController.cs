80774f9daee1cdab47c1881163495aa0b1b02bda
EngineerAhmedMo <engineer.ahmedhussein@gmail.com>
2019-02-05 20:22:21 +0200

    Upload function to upload ifc Files

--- WixBim/Controllers/HomeController.cs
+++ WixBim/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Xbim.Ifc;
+using Xbim.ModelGeometry.Scene;
 
 namespace WixBim.Controllers
 {
@@ -14,6 +19,58 @@ namespace WixBim.Controllers
             return View();
         }
 
+        public ActionResult Uploader()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Uploader(HttpPostedFileBase file)
+        {
+            if (file == null) return Content("Please Choose .ifc File and upload it");
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("../content/data"), fileName);
+                file.SaveAs(path);
+                ConvertToWexBIM(path);
+                ViewBag.Message = "File Uploaded Successfully";
+            }
+
+            return RedirectToAction("Viewer");
+            //return View();
+        }
+
+        private void ConvertToWexBIM(string filePath)
+        {
+            //  const string fileName = "../../my.ifc";
+            using (var model = IfcStore.Open(filePath))
+            {
+                var context = new Xbim3DModelContext(model);
+                context.CreateContext();
+
+
+                // physical full path in drive
+                var wexBimFullPath = Path.ChangeExtension(filePath, "wexBIM");
+
+                var wexBimFileName = Path.GetFileName(wexBimFullPath);
+
+                ConfigurationManager.AppSettings.Set("wexBIMFileName", wexBimFileName);
+                ConfigurationManager.AppSettings.Set("wexBIMFullPath", "../content/data/" + wexBimFileName);
+
+                using (var wexBiMfile = System.IO.File.Create(wexBimFullPath))
+                {
+                    using (var wexBimBinaryWriter = new BinaryWriter(wexBiMfile))
+                    {
+                        model.SaveAsWexBim(wexBimBinaryWriter);
+                        wexBimBinaryWriter.Close();
+                    }
+                    wexBiMfile.Close();
+                }
+
+            }
+        }
 
 
         public ActionResult About()
